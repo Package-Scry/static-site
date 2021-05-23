d4100acfe9b78f961cb23b0bed26b70d6ea19229
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2021-05-23 16:49:43 +0200

    connect newsletter subscription to endpoint

--- src/js/default.js
+++ src/js/default.js
@@ -35,7 +35,6 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   const { user } = data;
   const id = user?.id;
-
   const elMenuItem = document.querySelector("#menu-login");
   const userMenuItem = `<li id="menu-login"><span class="menu-item" >My account</span><span id="down-arrow">▼</span><div class="dropdown"><a class="dropdown-item" href="/subscriptions">My subscriptions</a><a class="dropdown-item" id="logout" href="https://package-scry.herokuapp.com/logout">Logout</a></div></li>`;
 
@@ -49,10 +48,49 @@ document.addEventListener("DOMContentLoaded", async function () {
       isLoggedIn = false;
     });
   } else {
-    const url = document.URL
+    const url = document.URL;
 
     if (url === "https://www.packagescry.com/contact-us/")
       window.location.href = "https://www.packagescry.com/login";
+
     isLoggedIn = false;
   }
+
+  const elSubscribeButton = document.querySelector("#subscribe-button");
+  const elSubscribeInput = document.querySelector("#subscribe-input");
+  const elSubscribeCheckbox = document.querySelector("#subscribe-checkbox");
+  const elSubscribeTerms = document.querySelector("#subscribe-terms");
+
+  elSubscribeCheckbox.addEventListener("change", () => {
+    if (elSubscribeTerms.style.color === "red")
+      elSubscribeTerms.style.color = "white";
+  });
+
+  elSubscribeButton.addEventListener("click", () => {
+    const isChecked = elSubscribeCheckbox.checked;
+    const emailValue = elSubscribeInput.value;
+
+    if (!isChecked) elSubscribeTerms.style.color = "red";
+
+    if (!!emailValue && isChecked) {
+      const response = await fetch(
+        "https://package-scry.herokuapp.com/post/subscribe",
+        {
+          credentials: "include",
+          headers: { "Content-Type": "application/json" },
+          method: "POST",
+          body: JSON.stringify({ email: emailValue }),
+        }
+      );
+
+      const data = await response.json();
+      const { status } = data;
+
+      if (status === "success")
+        elSubscribeInput.outerHTML = `<div style="color: white; font-size: 2em; text-align: center; margin: auto 0;">Success!</div>`;
+      else elSubscribeInput.value = "failed";
+
+      elSubscribeCheckbox.checked = false;
+    }
+  });
 });
