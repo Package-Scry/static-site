ddd150c51d01eac9f19a0bcb7c56c9619a941451
Kristof Szollosi <75531147+kristofszollosi@users.noreply.github.com>
2022-05-28 22:47:32 +0200

    add try catch to fetch

--- src/js/default.js
+++ src/js/default.js
@@ -27,71 +27,76 @@ document.addEventListener("DOMContentLoaded", async function () {
     elNav.className = elNav.className?.includes("active") ? "" : "active"
   })
 
-  const response = await fetch("https://package-scry.onrender.com/user", {
-    credentials: "include",
-  })
-
-  const data = await response.json()
-
-  const { user } = data
-  const id = user?.id
-  const elMenuItem = document.querySelector("#menu-login")
-  const userMenuItem = `<li id="menu-login"><span class="menu-item" >My account</span><span id="down-arrow">▼</span><div class="dropdown"><a class="dropdown-item" href="/subscriptions">My subscriptions</a><a class="dropdown-item" id="logout" href="https://package-scry.onrender.com/logout">Logout</a></div></li>`
-
-  if (id) {
-    elMenuItem.outerHTML = userMenuItem
-    isLoggedIn = true
-
-    const elLogout = document.querySelector("#logout")
-
-    elLogout.addEventListener("click", () => {
-      isLoggedIn = false
+  try {
+    const response = await fetch("https://package-scry.onrender.com/user", {
+      credentials: "include",
     })
-  } else {
-    const url = document.URL
 
-    if (url === "https://www.packagescry.com/contact-us/")
-      window.location.href = "https://www.packagescry.com/login"
+    const data = await response.json()
 
+    const { user } = data
+    const id = user?.id
+    const elMenuItem = document.querySelector("#menu-login")
+    const userMenuItem = `<li id="menu-login"><span class="menu-item" >My account</span><span id="down-arrow">▼</span><div class="dropdown"><a class="dropdown-item" href="/subscriptions">My subscriptions</a><a class="dropdown-item" id="logout" href="https://package-scry.onrender.com/logout">Logout</a></div></li>`
+
+    if (id) {
+      elMenuItem.outerHTML = userMenuItem
+      isLoggedIn = true
+
+      const elLogout = document.querySelector("#logout")
+
+      elLogout.addEventListener("click", () => {
+        isLoggedIn = false
+      })
+    } else {
+      const url = document.URL
+
+      if (url === "https://www.packagescry.com/contact-us/")
+        window.location.href = "https://www.packagescry.com/login"
+
+      isLoggedIn = false
+    }
+
+    const elSubscribeButton = document.querySelector("#subscribe-button")
+    const elSubscribeInput = document.querySelector("#subscribe-input")
+    const elSubscribeCheckbox = document.querySelector("#subscribe-checkbox")
+    const elSubscribeTerms = document.querySelector("#subscribe-terms")
+
+    elSubscribeCheckbox.addEventListener("change", () => {
+      if (elSubscribeTerms.style.color === "red")
+        elSubscribeTerms.style.color = "white"
+    })
+
+    elSubscribeButton.addEventListener("click", async () => {
+      const isChecked = elSubscribeCheckbox.checked
+      const emailValue = elSubscribeInput.value
+
+      if (!isChecked) elSubscribeTerms.style.color = "red"
+
+      if (!!emailValue && isChecked) {
+        const response = await fetch(
+          "https://package-scry.onrender.com/post/subscribe",
+          {
+            credentials: "include",
+            headers: { "Content-Type": "application/json" },
+            method: "POST",
+            body: JSON.stringify({ email: emailValue }),
+          }
+        )
+
+        elSubscribeInput.value = ""
+        const data = await response.json()
+        const { status } = data
+
+        if (status === "success")
+          elSubscribeInput.outerHTML = `<div style="color: white; font-size: 2em; text-align: center; margin: auto 0;">Success!</div>`
+        else elSubscribeInput.value = "failed"
+
+        elSubscribeCheckbox.checked = false
+      }
+    })
+  } catch (e) {
+    console.log("YOLO")
     isLoggedIn = false
   }
-
-  const elSubscribeButton = document.querySelector("#subscribe-button")
-  const elSubscribeInput = document.querySelector("#subscribe-input")
-  const elSubscribeCheckbox = document.querySelector("#subscribe-checkbox")
-  const elSubscribeTerms = document.querySelector("#subscribe-terms")
-
-  elSubscribeCheckbox.addEventListener("change", () => {
-    if (elSubscribeTerms.style.color === "red")
-      elSubscribeTerms.style.color = "white"
-  })
-
-  elSubscribeButton.addEventListener("click", async () => {
-    const isChecked = elSubscribeCheckbox.checked
-    const emailValue = elSubscribeInput.value
-
-    if (!isChecked) elSubscribeTerms.style.color = "red"
-
-    if (!!emailValue && isChecked) {
-      const response = await fetch(
-        "https://package-scry.onrender.com/post/subscribe",
-        {
-          credentials: "include",
-          headers: { "Content-Type": "application/json" },
-          method: "POST",
-          body: JSON.stringify({ email: emailValue }),
-        }
-      )
-
-      elSubscribeInput.value = ""
-      const data = await response.json()
-      const { status } = data
-
-      if (status === "success")
-        elSubscribeInput.outerHTML = `<div style="color: white; font-size: 2em; text-align: center; margin: auto 0;">Success!</div>`
-      else elSubscribeInput.value = "failed"
-
-      elSubscribeCheckbox.checked = false
-    }
-  })
 })
